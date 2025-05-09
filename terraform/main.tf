ff1c1fb3db37e59a20c4fa8157ceab633d1d43d8
Haripriya2408 <haripriyaveluchamy@gmail.com>
2025-05-09 21:51:45 +0530

    k8 version configured

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.96.0"
+      version = "~> 3.75.0"
     }
   }
   backend "azurerm" {
@@ -16,6 +16,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = false
+
 }
 
 module "aks" {
